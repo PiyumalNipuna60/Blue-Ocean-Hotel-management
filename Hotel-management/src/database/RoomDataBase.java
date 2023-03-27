28be0a357669e4e46a726f84f55b3855075533e1
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-27 23:53:45 +0530

    Add RoomDataBase class

---
+++ Hotel-management/src/database/RoomDataBase.java
@@ -0,0 +1,13 @@
+package database;
+
+import model.Room;
+
+import java.util.ArrayList;
+
+public class RoomDataBase {
+    public static ArrayList<Room>RoomTable = new ArrayList<Room>();
+
+    static {
+
+    }
+}
