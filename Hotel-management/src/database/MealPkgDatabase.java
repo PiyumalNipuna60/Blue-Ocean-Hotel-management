f335346be50d936a84fd178686450145f60a87ee
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-27 23:53:23 +0530

    Add MealPkgDatabase class

---
+++ Hotel-management/src/database/MealPkgDatabase.java
@@ -0,0 +1,13 @@
+package database;
+
+import model.Meal;
+
+import java.util.ArrayList;
+
+public class MealPkgDatabase {
+    public static ArrayList<Meal>MealPkgTable = new ArrayList<Meal>();
+
+    static {
+
+    }
+}
