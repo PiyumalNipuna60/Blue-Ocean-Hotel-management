13ae54006de59cbb04f1540dcd3477e0ffa8a759
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-27 23:53:08 +0530

    Add GustDatabase class

---
+++ Hotel-management/src/database/GustDatabase.java
@@ -0,0 +1,13 @@
+package database;
+
+//import model.Gust;
+
+import java.util.ArrayList;
+
+public class GustDatabase {
+   //public static ArrayList<Gust>gustTable = new ArrayList<Gust>();
+
+   static {
+
+   }
+}
