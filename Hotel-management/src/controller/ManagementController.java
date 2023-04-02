d04c6161e82dfb1b071edd8386486ae5c397a30b
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-04-02 23:41:53 +0530

    Add ManagementController class

---
+++ Hotel-management/src/controller/ManagementController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ManagementController {
+
+
+    public Button btnRoomDetails;
+    public Button btnMealDetails;
+    public Button btnIncomDetails;
+    public Button btnBack1;
+
+    public void RoomDetailsOnAction(ActionEvent actionEvent) throws IOException {
+
+    }
+
+    public void MealDetailsOnAction(ActionEvent actionEvent) throws IOException {
+
+    }
+
+    public void IncomeDetailsOnAction(ActionEvent actionEvent) throws IOException {
+
+    }
+
+    public void BackOnActio9n(ActionEvent actionEvent) throws IOException {
+
+    }
+}
